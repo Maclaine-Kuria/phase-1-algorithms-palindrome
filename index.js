512e561302f1af5faf6aa404bfdd7f7e8381c7a1
maclaine <maclainekuria@gmail.com>
2022-06-17 14:35:53 +0300

    commit tree b877d124a241d35772c926d3e3de5453ea98c262

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  return word === reversedWord;
 }
 
 /* 
@@ -20,6 +27,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
